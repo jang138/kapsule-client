5ede8464300bb6bdf4a4d66374584d6f66c989a9
Kim-Hyo-Jeong <lk7938@naver.com>
2024-08-21 16:59:21 +0900

    add PageNotFound design
    PageNotFound 페이지 생성

--- src/router/route.js
+++ src/router/route.js
@@ -1,7 +1,23 @@
-export default [
-    {
-        path: "/",
+import MiddleFront from '@/components/layout/MiddleFront.vue';
+import PageMain from '@/components/pages/PageMain.vue';
+import PageNotFound from '@/components/pages/PageNotFound.vue';
 
-        children: [],
+const routes = [
+    {
+        path: '/',
+        component: MiddleFront,
+        children: [
+            {
+                path: 'main',
+                component: PageMain,
+                alias: ['/'],
+            },
+            {
+                path: '/:catchAll(.*)',
+                component: PageNotFound,
+            },
+        ],
     },
 ];
+
+export default routes;
