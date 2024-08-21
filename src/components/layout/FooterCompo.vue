a152913795c4ff0d25ec2125763fed181f1d9e79
Kim-Hyo-Jeong <lk7938@naver.com>
2024-08-21 12:03:26 +0900

    add header, footer design
    Header와 Footer의 디자인 추가

--- src/components/layout/FooterCompo.vue
+++ src/components/layout/FooterCompo.vue
@@ -1,11 +1,65 @@
 <template>
     <div class="footer">
-        <h3>Footer</h3>
+        <button class="button_add">
+            <svg
+                class="icon_plus"
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="currentColor"
+                stroke-width="2"
+            >
+                <path d="M19 13H13V19H11V13H5V11H11V5H13V11H19V13Z" />
+            </svg>
+        </button>
     </div>
 </template>
 
 <script>
-export default {};
+export default {
+    name: 'FooterCompo',
+};
 </script>
 
-<style scoped></style>
+<style scoped>
+.footer {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    background-color: #373a40;
+    user-select: none;
+    height: 80px;
+}
+
+.button_add {
+    width: 50px;
+    height: 50px;
+    border: none;
+    border-radius: 50%;
+    background-color: #eeeeee;
+    color: #373a40;
+    font-weight: bold;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    cursor: pointer;
+    box-shadow: 0px 4px 8px rgba(0, 0, 0, 0.2);
+    transition: background-color 0.3s, box-shadow 0.3s;
+}
+
+.button_add .icon_plus {
+    width: 24px; /* 아이콘의 크기 조정 */
+    height: 24px;
+}
+
+.button_add:hover {
+    background-color: #bbbbbb;
+    box-shadow: 0px 6px 12px rgba(0, 0, 0, 0.3);
+}
+
+.button_add:active {
+    background-color: #999999;
+    box-shadow: 0px 2px 4px rgba(0, 0, 0, 0.3);
+    transform: scale(0.95);
+}
+</style>
--- src/components/layout/HeaderCompo.vue
+++ src/components/layout/HeaderCompo.vue
@@ -1,16 +1,47 @@
 <template>
-    <div class="header" style="display: flex">
-        <h3>Header</h3>
+    <div class="header">
+        <svg class="icon_mypage" xmlns="http://www.w3.org/2000/svg" fill="#ffffff" viewBox="0 0 24 24">
+            <path
+                d="M12 12c2.21 0 4-1.79 4-4s-1.79-4-4-4-4 1.79-4 4 1.79 4 4 4zm0 2c-2.67 0-8 1.34-8 4v2h16v-2c0-2.66-5.33-4-8-4z"
+            />
+        </svg>
+
+        <h2 class="title_header">Kapsule</h2>
+
+        <img class="icon_kakao" src="@/assets/icon_kakao.png" alt="KakaoTalk Icon" />
     </div>
 </template>
 
 <script>
-
 export default {
-
+    name: 'HeaderCompo',
 };
 </script>
 
 <style scoped>
+.header {
+    align-items: center;
+    justify-content: space-between;
+    background-color: #373a40;
+    user-select: none;
+}
 
+.icon_mypage {
+    width: 40px;
+    height: 40px;
+    padding: 20px;
+    fill: #ffffff;
+}
+
+.title_header {
+    color: #ffffff;
+    font-family: 'Piedra', cursive;
+    margin: 0;
+}
+
+.icon_kakao {
+    width: 40px;
+    height: 40px;
+    padding: 20px;
+}
 </style>
