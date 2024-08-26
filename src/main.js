06290488a58f59ddc3060a53ac82789172d4e7ed
dae <cys964@gmail.com>
2024-08-26 09:29:34 +0900

    main.js pinia 등록

--- src/main.js
+++ src/main.js
@@ -1,9 +1,13 @@
-import pinia from '@/components/stores/index';
-import { createApp } from 'vue';
-import App from './App.vue';
-import router from './router';
+import { createApp } from "vue";
+import { createPinia } from "pinia"; // Pinia를 가져오기
+import App from "./App.vue";
+import router from "./router";
 
 const app = createApp(App);
-app.use(pinia);
+
+const pinia = createPinia(); // Pinia 인스턴스 생성
+
+app.use(pinia); // Pinia를 앱에 등록
 app.use(router);
-app.mount('#app');
+
+app.mount("#app");
