6327525634aaef5f06f10f18eff5edfa50aa3b05
이다영 <yooung513@gmail.com>
2024-08-27 19:27:33 +0900

    Create 환경 설정

---
+++ src/axios.js
@@ -0,0 +1,10 @@
+import axios from 'axios';
+
+const axiosInstance = axios.create({
+    // 프록시 설정을 사용하여 /api로 시작하는 모든 요청은 Spring Boot 서버로 전달됨
+    baseURL: '/api',
+    // 인증 관련 설정이 필요한 경우
+    withCredentials: true,
+});
+
+export default axiosInstance;
