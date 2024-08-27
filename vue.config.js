9e76d7141dabbacfb9eb5e230bd062688166cade
이다영 <106363495+yooung513@users.noreply.github.com>
2024-08-27 19:31:18 +0900

    Create 환경 설정 (#20)

--- vue.config.js
+++ vue.config.js
@@ -3,5 +3,12 @@ module.exports = defineConfig({
     transpileDependencies: true,
     devServer: {
         https: true, // HTTPS 사용
+        proxy: {
+            '/api': {
+                target: 'http://localhost:8088', // Spring Boot 서버의 기본 URL
+                changeOrigin: true,
+                pathRewrite: { '^/api': '' }, // API 요청 경로에서 '/api' 제거
+            },
+        },
     },
 });
